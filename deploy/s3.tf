b7049dcfbda9df826cfef1f0e1ce66136e215168
san14258 <s_santi126@hotmail.com>
2025-11-26 18:22:10 -0300

    Se modifica template para desplegar con autoscaling. Instala apache+php según readme.md. Se pasan paraemtros de configuración para conectar con endpoint de la base de datos. Se genera aplicacion en .zip para desplegar en EC2

--- deploy/s3.tf
+++ deploy/s3.tf
@@ -7,11 +7,35 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+resource "aws_s3_bucket" "app_bucket" {
+  bucket        = "${var.vpc_name}-app-bucket"
+  force_destroy = true
+
+  tags = {
+    Name = "${var.vpc_name}-app-bucket"
+  }
+}
+
 resource "aws_s3_object" "dump_sql" {
   bucket = aws_s3_bucket.bucket.id
   key    = "dump.sql"
 
   # Ruta relativa desde el módulo "deploy"
-  source = "/home/sysadmin/OBG-SolucionesCloud/app/php-ecommerce-obligatorio-2025/dump.sql"
+  source = "/root/OBG-SolucionesCloud/app/php-ecommerce-obligatorio-2025/dump.sql"
 
+}
+
+# Zipea la App completa
+data "archive_file" "app_zip" {
+  type        = "zip"
+  source_dir  = "/root/OBG-SolucionesCloud/app/php-ecommerce-obligatorio-2025"
+  output_path = "${path.module}/app.zip"
+}
+# Sube el zip al bucket
+resource "aws_s3_object" "app_zip" {
+  bucket = aws_s3_bucket.app_bucket.id
+  key    = "app.zip"
+  source = data.archive_file.app_zip.output_path
+
+  etag = filemd5(data.archive_file.app_zip.output_path)
 }
